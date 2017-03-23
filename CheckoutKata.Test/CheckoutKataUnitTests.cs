4ed38d7b3b8e64753fd592a0a350f0ac2fa6b62c
WEBDEV <WEBDEV@WEBDEV>
2017-03-23 23:23:46 +0000

    Add test for a different item. This should fail.

--- CheckoutKata.Test/CheckoutKataUnitTests.cs
+++ CheckoutKata.Test/CheckoutKataUnitTests.cs
@@ -66,6 +66,20 @@ namespace CheckoutKata.Test
         }
 
         [TestMethod]
+        public void Basket_Contains_Bx1_TotalPrice_Is_30()
+        {
+            //Arrange
+            var item = "B";
+
+            //Act
+            checkout.Scan(item);
+            var totalPrice = checkout.GetTotalPrice();
+
+            //Assert
+            Assert.AreEqual(30, totalPrice);
+        }
+
+        [TestMethod]
         public void CreateBasket()
         {
             //Arrange
