db308d170f16d6527b17f791dae317ece48c387d
WEBDEV <WEBDEV@WEBDEV>
2017-03-23 21:43:34 +0000

    Add project files.

---
+++ CheckoutKata.Test/CheckoutKataUnitTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutKata;
+
+namespace CheckoutKata.Test
+{
+    [TestClass]
+    public class CheckoutKataUnitTests
+
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
