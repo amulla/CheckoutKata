54f8c9cf44418e52eae9cf0cd7bfabef1aa63cd9
WEBDEV <WEBDEV@WEBDEV>
2017-03-23 23:22:41 +0000

    Hard code the prices in the price list implementation. This would normally use a price list service or repository.

--- CheckoutKata/StandardPriceList.cs
+++ CheckoutKata/StandardPriceList.cs
@@ -8,9 +8,28 @@ namespace CheckoutKata
 {
     public class StandardPriceList : IPriceList
     {
+        private List<Price> prices;
+
+        public StandardPriceList()
+        {
+            prices = new List<Price>();
+
+            prices.Add(new Price() { item = "A", quantity = 1, price = 50 });
+            prices.Add(new Price() { item = "A", quantity = 3, price = 130 });
+        }
+
         public int GetPrice(string item, int quantity)
         {
-            return 50 * quantity;
+            Price price = prices.Find(p => p.item == item && p.quantity == quantity);
+
+            if (price != null)
+            {
+                return price.price;
+            }
+            else
+            {
+                return prices.Find(p => p.item == item && p.quantity == 1).price * quantity;
+            }
         }
     }
 }
