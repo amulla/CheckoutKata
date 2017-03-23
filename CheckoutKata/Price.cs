13251c7842e5b9497a070c6d5fa3d7ceee72b1cb
WEBDEV <WEBDEV@WEBDEV>
2017-03-23 23:19:17 +0000

    Add a price POCO to hold individual prices

---
+++ CheckoutKata/Price.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata
+{
+    public class Price
+    {
+        public string item { get; set; }
+        public int quantity { get; set; }
+        public int price { get; set; }
+    }
+}
